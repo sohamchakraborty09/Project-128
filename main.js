ea0831f05672169bfa94b97156a5d713192cf29b
sohamchakraborty09 <86429132+sohamchakraborty09@users.noreply.github.com>
2022-09-23 08:37:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+var song = " ";
+var song = " ";
+var leftWristX = 0;
+var leftWristY = 0;
+var rightWristX = 0;
+var rightWristY = 0;
+function preload(){
+    song = loadSound("music.mp3");
+}
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+function gotPoses(results){
+if(results.length > 0){
+    console.log(results);
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX ="+leftWristX+"leftWristY = "+leftWristY);
+
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristy = results[0].pose.rightWrist.y;
+    console.log("rightWristX ="+rightWristX+"rightWristY = "+rightWristY);
+}
+}
